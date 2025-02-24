c649c2f4d306d349f2e00bad69379ba668bdb8d8
morlabhaskar <morlabhaskar306@gmail.com>
2025-02-24 13:31:14 +0530

    after dark toggle mode future

--- config/mongodb.js
+++ config/mongodb.js
@@ -5,6 +5,5 @@ const connectDB = async () => {
     mongoose.connection.on('connected',()=> console.log("Database is Connected"))
 
     await mongoose.connect(`${process.env.MONGO_URL}`)
-    // await mongoose.connect(`${process.env.MONGO_URL}/doctor-appointment`)
 }
 export default connectDB
